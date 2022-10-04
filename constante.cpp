5bea7d35d6fcfc75ddc456e6dba1ca721d0f831f
Juan Carcamo <pablocarcamo2004@gamil.com>
2022-10-04 09:40:41 -0600

    se agregó de nuevo el archivo constante.cpp

---
+++ constante.cpp
@@ -0,0 +1,16 @@
+/*
+nombre del archivo: sumanumeros.cpp
+autor: Juan Carcamo
+Lugar: ITV
+Instruciones: Esto solo es una plantilla
+*/
+ #include<iostream>
+ 
+ usig nameespace std;
+ 
+ int main(){
+
+return 0;
+ 	
+ 	
+ }
